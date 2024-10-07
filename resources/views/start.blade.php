f4d5138023733d93489ce02436e05e5e6e67e915
Fabian Walter <fabianwalter8@gmail.com>
2024-10-07 22:38:36 +0200

    removed welcome, added start

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,34 +1,31 @@
 <x-startuse-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        @if (Route::has('login'))
-                            <nav class="-mx-3 flex gap-2 flex-1 justify-end">
-                                @auth
-                                    <a
-                                        href="{{ url('/dashboard') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Dashboard
-                                    </a>
-                                @else
-                                    <a
-                                        href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white bg-blue-500/80 dark:hover:text-black dark:focus-visible:ring-white border border-blue-500/80 hover:bg-transparent border-10"
-                                    >
-                                        Log in
-                                    </a>
+            @if (Route::has('login'))
+            <nav class="-mx-3 flex gap-2 flex-1 justify-end">
+                @auth
+                <a
+                    href="{{ url('/dashboard') }}"
+                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                    Dashboard
+                </a>
+                @else
+                <a
+                    href="{{ route('login') }}"
+                    class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white bg-blue-500/80 dark:hover:text-black dark:focus-visible:ring-white border border-blue-500/80 hover:bg-transparent border-10">
+                    Log in
+                </a>
 
-                                    @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white bg-blue-500/80 dark:hover:text-black dark:focus-visible:ring-white border border-blue-500/80 hover:bg-transparent border-10"
-                                        >
-                                            Register
-                                        </a>
-                                    @endif
-                                @endauth
-                            </nav>
-                        @endif
+                @if (Route::has('register'))
+                <a
+                    href="{{ route('register') }}"
+                    class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white bg-blue-500/80 dark:hover:text-black dark:focus-visible:ring-white border border-blue-500/80 hover:bg-transparent border-10">
+                    Register
+                </a>
+                @endif
+                @endauth
+            </nav>
+            @endif
         </div>
     </div>
 </x-startuse-layout>
