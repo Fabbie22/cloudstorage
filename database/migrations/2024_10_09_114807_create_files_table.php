7d6af101185eca6e5f7faaaf3eccb261d1f94c15
Fabian Walter <fabianwalter8@gmail.com>
2024-10-09 16:50:30 +0200

    updated database

--- database/migrations/2024_10_09_114807_create_files_table.php
+++ database/migrations/2024_10_09_114807_create_files_table.php
@@ -11,39 +11,30 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Create the 'files' table first
         Schema::create('files', function (Blueprint $table) {
             $table->id();
             $table->string('path');
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
 
-        // Create the 'shared' table next
         Schema::create('shared', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('file_id'); // Use unsignedBigInteger to match the 'id' type in files table
+            $table->unsignedBigInteger('file_id');
             $table->string('owner_email');
             $table->string('recipient_email');
 
-            // Foreign key constraint
             $table->foreign('file_id')->references('id')->on('files')->onDelete('cascade');
         });
 
-        // Create the 'users_has_files' table next
         Schema::create('users_has_files', function (Blueprint $table) {
             $table->unsignedBigInteger('users_id');
-            $table->unsignedBigInteger('files_id'); // Use unsignedBigInteger to match the 'id' type in files table
+            $table->unsignedBigInteger('files_id');
             $table->primary(['users_id', 'files_id']);
             $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('files_id')->references('id')->on('files')->onDelete('cascade');
         });
 
-        // Create the 'deleted_file_properties' table last
-        Schema::create('deleted_file_properties', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->timestamp('deleted_at')->nullable();
-        });
     }
 
     /**
