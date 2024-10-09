228ef4990f262d8f7a2446c4a0f8f88b8f2d2be9
Fabian Walter <fabianwalter8@gmail.com>
2024-10-09 17:25:51 +0200

    update with seeder for user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run() : void
+    {
+        $faker = Faker::create();
+    
+        // Fetch all valid role IDs from the 'roles' table
+        $roleIds = DB::table('roles')->pluck('id')->toArray();
+
+        
+        // Loop 100 times to create 100 fake users
+        for ($i = 0; $i < 100; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->userName(),
+                'email' => $faker->unique()->safeEmail(),
+                'password' => Hash::make('password'),
+                'role_id' => $faker->randomElement($roleIds), // Assign a valid role_id
+            ]);
+        }
+    }
+    }
