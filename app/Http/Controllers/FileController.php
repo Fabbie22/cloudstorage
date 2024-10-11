4cce57f5c06dbf7ac0f82863b3e5109766273a64
Fabian Walter <fabianwalter8@gmail.com>
2024-10-11 11:55:23 +0200

    updated file to user

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\File;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth; // If using Auth facade for authenticated user
+use Illuminate\Support\Facades\Storage; // If handling file storage
+use Illuminate\Http\Response; // If you need to manage file downloads/responses
+
 
 class FileController extends Controller
 {
@@ -18,7 +21,7 @@ class FileController extends Controller
 
     public function read() 
     {
-        $files = File::all();
+        $files = File::where('user_id', auth()->id())->get();
         return view('files.files', compact('files'));
     }
 
