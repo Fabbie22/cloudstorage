4bb6bf2aa64579369e54ece90f0f1472a7b59c63
Fabian Walter <fabianwalter8@gmail.com>
2024-10-14 13:49:41 +0200

    updated notification info

--- resources/views/files/partials/notification-info.blade.php
+++ resources/views/files/partials/notification-info.blade.php
@@ -1,26 +1,37 @@
 <section>
-    @if (session('status') === 'file-shared')
-        <div class="w-full bg-green-500 p-5 rounded-lg mb-4" x-data="{ show: true }" x-show="show" x-transition
-            x-init="setTimeout(() => show = false, 2000)">
-            <p class="text-lg font-semibold text-white">{{ __('File Shared!') }}</p>
-        </div>
-    @endif
-    @if (session('status') === 'already-shared')
-        <div class="w-full bg-red-500 p-5 rounded-lg mb-4" x-data="{ show: true }" x-show="show" x-transition
-            x-init="setTimeout(() => show = false, 2000)">
-            <p class="text-lg font-semibold text-white">{{ __('File is already shared with the user!') }}</p>
-        </div>
-    @endif
-    @if (session('status') === 'files-uploaded')
-        <div class="w-full bg-green-500 p-5 rounded-lg mb-4" x-data="{ show: true }" x-show="show" x-transition
-            x-init="setTimeout(() => show = false, 2000)">
-            <p class="text-lg font-semibold text-white">{{ __('File uploaded!') }}</p>
-        </div>
-    @endif
-        @if (session('status') === 'files-deleted')
-        <div class="w-full bg-green-500 p-5 rounded-lg mb-4" x-data="{ show: true }" x-show="show" x-transition
-            x-init="setTimeout(() => show = false, 2000)">
-            <p class="text-lg font-semibold text-white">{{ __('File deleted successfully!') }}</p>
-        </div>
-    @endif
+    @php
+        $status = session('status');
+    @endphp
+
+    <div x-data="{ show: true }" x-show="show" x-transition
+         x-init="setTimeout(() => show = false, 2000)">
+        @switch($status)
+            @case('file-shared')
+                <div class="w-full bg-green-500 p-5 rounded-lg mb-4">
+                    <p class="text-lg font-semibold text-white">{{ __('File Shared!') }}</p>
+                </div>
+                @break
+
+            @case('already-shared')
+                <div class="w-full bg-red-500 p-5 rounded-lg mb-4">
+                    <p class="text-lg font-semibold text-white">{{ __('File is already shared with the user!') }}</p>
+                </div>
+                @break
+
+            @case('files-uploaded')
+                <div class="w-full bg-green-500 p-5 rounded-lg mb-4">
+                    <p class="text-lg font-semibold text-white">{{ __('File uploaded!') }}</p>
+                </div>
+                @break
+
+            @case('files-deleted')
+                <div class="w-full bg-green-500 p-5 rounded-lg mb-4">
+                    <p class="text-lg font-semibold text-white">{{ __('File deleted successfully!') }}</p>
+                </div>
+                @break
+
+            @default
+                {{-- Optionally handle a default case here if needed --}}
+        @endswitch
+    </div>
 </section>
