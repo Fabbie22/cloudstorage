046d89378db4a6dd49ce438407279d96bfe1276b
Fabian Walter <fabianwalter8@gmail.com>
2024-10-14 10:20:51 +0200

    update migration and model for soft delete

--- database/migrations/2024_10_09_114807_create_files_table.php
+++ database/migrations/2024_10_09_114807_create_files_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('path');
             $table->timestamps();
-            $table->timestamp('deleted_at')->nullable();
+            $table->softDeletes();
             $table->unsignedBigInteger('user_id');
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
