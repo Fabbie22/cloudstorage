b99595bc143bc4468cd07e7f10e47dbe4935b611
Fabian Walter <fabianwalter8@gmail.com>
2024-10-16 16:07:57 +0200

    removed comment

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ Route::get('/', function () {
     return view('start');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth', 'verified')->group(function () {
     Route::get('/dashboard', [DataController::class, 'dashboard'])->name('dashboard');
 });
